4d0dab5220bd7f430fd9f6e8fe35aa73381e92ca
dofajka <marko.hassinen@helsinki.fi>
2018-03-08 13:28:26 +0200

    sigma and mu finder func

--- plots/randomized_k_portfolios.py
+++ plots/randomized_k_portfolios.py
@@ -43,18 +43,115 @@ def markowitz_randomized(n, k, size, mean, cov, lambd, mu0, sigma0):
 	markowitz = options[np.random.choice(len(options))]
 	return markowitz, np.array([randomized_k_portfolios(n, k, markowitz, size) for i in range(size)])
 
+
+
 def asset_update(k, p, mean, cov, mu0, sigma0):
 	n = mean.shape[0]
 	RP = randomized_k_portfolios(n, k, p, size=1000)
+	mu1, sigma1 = update_return_risk(RP, mu0, sigma0)
+
+def asset_update_old(k, p, mean, cov, mu0, sigma0):
+	# Number of assets
+	n = mean.shape[0]
+	# Size amout of random portfolios drawn with the distribution of p 
+	RP = randomized_k_portfolios(n, k, p, size=1000)
+	# Risks and returns of random portfolios RP
 	risks, returns = cvxpy_fit(mean, cov, RP)
+	# Initialization of intersection of RP and interesting region IR {(risk, return) : risk < sigma0 and return > mu0}
 	RPIR = np.array([])
+	# Method to find the set objects of RPIR
 	for i in range(len(RP)):
 		if returns[i] > mu0 and risks[i] < sigma0:
 			if RPIR.shape[0] == 0:
 				RPIR = np.array([RP[i]])
 			else:
 				RPIR = np.append(RPIR, [RP[i]], axis=0)
-	p = np.zeros(n)
-	for i in range(n):
-		p[i] = RPIR[RPIR[:, i] > 0].shape[0] / RPIR.shape[0]
-	return RPIR, p
+	# New asset distribution p_new initialization
+	p_new = np.zeros(n)
+	# Cardinality of RPIR
+	m = RPIR.shape[0]
+	# if statement to evade zero division error within this clause
+	if m > 0:
+		# p_new update method
+		for i in range(n):
+			z = 0
+			for r in RPIR:
+				if r[i] > 0:
+					z += 1
+		#p[i] = RPIR[RPIR[:, i] > 0].shape[0] / m
+			p_new[i] = z / m
+	# Normalization of p_new					
+	if not p_new.sum() == 0: 
+		p_new /= p_new.sum()
+	else:
+		p_new = p
+	return RPIR, p_new
+
+def g(risks, returns, alpha):
+	min_risk = risks.min()
+	max_risk = risks.max()
+	min_return = returns.min()
+	beta = 0
+	beta += alpha
+	inside_risks = []
+	inside_returns = [r for r in returns]
+	outside_returns = []
+	outside_risks = []
+	while(len(inside_returns) > 800):
+		inside_risks = []
+		inside_returns = []
+		outside_returns = []
+		outside_risks = []
+		for ri, re in zip(risks, returns):
+			s0 = max_risk*(1-beta)
+			m0 = min_return*(1+beta)
+			if s0 > ri and m0 < re:
+				inside_risks.append(ri)
+				inside_returns.append(re)
+			else:
+				outside_returns.append(re)
+				outside_risks.append(ri)
+		if(len(inside_returns) > 800):
+			beta += alpha
+		else:
+			beta -= alpha
+	sigma = max_risk*(1-beta)
+	mu = min_return*(1+beta)
+	return sigma, mu, beta
+	'''
+	inside_risks = []
+	inside_returns = []
+	outside_returns = []
+	outside_risks = []
+	for ri, re in zip(risks, returns):
+		s0 = max_risk*(1-beta)
+		m0 = min_return*(1+beta)
+		if s0 > ri and m0 < re:
+			inside_risks.append(ri)
+			inside_returns.append(re)
+		else:
+			outside_returns.append(re)
+			outside_risks.append(ri)		
+	plt.plot(inside_risks, inside_returns, 'bo')
+	plt.plot(outside_risks, outside_returns, 'ro')
+	plt.axhline(mu)
+	plt.axvline(sigma)
+	plt.show()
+	return sigma, mu, beta'''
+
+'''
+j = 0
+r1 = []
+r2 = []
+for i in range(100):
+	print(i)
+	RPIR1, p1 = asset_update(10, p0, mean, cov, 0.001, 0.018) 
+	RPIR2, p2 = asset_update(10, p1, mean, cov, 0.001, 0.018) 
+	i1 = RPIR1.shape[0] / 1000
+	i2 = RPIR2.shape[0] / 1000
+	r1.append(i1)
+	r2.append(i2)
+	if i2 > i1:
+		j += 1
+j / 100
+'''
